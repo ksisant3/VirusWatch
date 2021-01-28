3b9f23b973e982b3c8dbb32115f63cf0dde2f9a2
bstahman <bjs492@nau.edu>
2021-01-28 11:10:15 -0700

    added file upload to db

--- project/VirusWatch/VWlogin/settings.py
+++ project/VirusWatch/VWlogin/settings.py
@@ -77,11 +77,6 @@ WSGI_APPLICATION = 'VWlogin.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 # MYSQL DB
     'default': {
